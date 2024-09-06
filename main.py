bbd3e834c5f2b766da6c556196df272ae9b9046c
The Mechanist <zer0inyourface@gmail.com>
2024-09-06 15:39:15 +0530

    added spacing algo() for responsive ui

--- main.py
+++ main.py
@@ -5,21 +5,37 @@ class List:
     tasks = []
     @staticmethod
     def add_task():
+        id = input("Enter a temp id: ")
         title = input("Pls enter a title for task: ")
         date = datetime.datetime.now()
         priority = input("pls enter priority for this task: ")
 
-        return Task(title, date, priority)
+        return Task(id, title, date, priority)
+
+    @staticmethod
+    def spacing_algo(text, t):
+        length = int(len(text))
+        total_length = t
+        total_spaces = int((total_length - length)/2)
+        if (total_length - length) % 2 != 0:
+            return ' ' * total_spaces + text + ' ' * (total_spaces+1)
+        return ' ' * total_spaces + text + ' ' * total_spaces
 
     @staticmethod
     def view_tasks():
+        print('-'*65)
+        print("|  id  "+"|"+" "*20+"task"+" "*20+"|"+" "*2+"priority"+" "*1+"|")
+        print('-'*65)
+        id_spaces = 6
+        task_spaces = 44
+        priority_spaces = 11
         for items in List.tasks:
-            print(f'{len(items.title) * "-"}')
-            print(items.title)
-
+            print('|' + List.spacing_algo(f"{items.id}", id_spaces)+'|'+List.spacing_algo(f"{items.title}", task_spaces)+'|'+List.spacing_algo(f"{items.priority}", priority_spaces)+'|')
+            print('-'*65)
 
 class Task:
-    def __init__(self, title, date, priority="High", status="ongoing"):
+    def __init__(self, id, title, date, priority="High", status="ongoing"):
+        self.id = id
         self.title = title
         self.status = status
         self.date = date
@@ -44,4 +60,5 @@ if __name__ == '__main__':
         elif user_input == 'view_tasks':
             List.view_tasks()
         elif user_input == '/quit':
+            os.system('cls')
             break
